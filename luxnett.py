1d77e7865eb3d4acceecdb306cdb2786ed62988e
bubu <romain.buret@imerir.com>
2018-02-05 18:47:12 +0100

    Liaison front back activation désactivation bouton

--- luxnett.py
+++ luxnett.py
@@ -1,5 +1,5 @@
-from flask import Flask , render_template
-
+from flask import Flask, render_template, request, make_response
+import json
 
 app = Flask(__name__)
 
@@ -9,6 +9,32 @@ def hello_world():
     return render_template('index.html')
 
 
+@app.route('/brush/', methods=['POST'])
+def brush():
+    post = request.get_json(force=True)
+    print(post['state'])
+    if post['state'] == 1:
+        print("enable brush")
+    elif post['state'] == 0:
+        print("disable brush")
+    else:
+        print("error")
+        return make_response('400')
+    return make_response('200')
+
+@app.route('/water/', methods=['POST'])
+def water():
+    post = request.get_json(force=True)
+    print(post['state'])
+    if post['state'] == 1:
+        print("enable water")
+    elif post['state'] == 0:
+        print("disable water")
+    else:
+        print("error")
+        return make_response('400')
+    return make_response('200')
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0")
