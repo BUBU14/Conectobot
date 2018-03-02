5a2fcc5891977c56d6fca2d21f3f57e87e8fad3b
bubu14 <romainburet14@gmail.com>
2018-03-02 10:27:15 +0100

    suprr ROS

--- automatic.py
+++ automatic.py
@@ -1,8 +1,43 @@
 # ROS
-#!/usr/bin/env python
 
 def mainAuto(width,height,speed):
     print("i'm start auto")
-    print("make a ROS for :\n \t width\t:"+width+"\n \t height\t:"+height+"\n \t speed\t:"+speed)
+    print("clean restriction:\n \t width\t:"+width+"\n \t height\t:"+height+"\n \t speed\t:"+speed)
+    calculDuration(width,height,speed)
+    start()
+    end()
+
+def start():
+    moveInfo = "LR"
+    capteurL = 1
+    capteurR = 1
+    capteurH = 1
 
 
+    while (capteurL != 0 or capteurR != 0 or capteurH != 0):
+        move()
+    if capteurH:
+        if moveInfo =="LR":
+            rotation(90,"R")
+            moveInfo = "RL"
+
+        else :
+            rotation(90,"L")
+            moveInfo = "LR"
+
+
+
+def rotation(angle, direction):
+    print("i'm turn")
+
+def move():
+    print("i'm move")
+    # activation moteur
+
+def calculDuration(width, height,speed):
+    duration =(width * height)/speed
+
+    print("Duration of clean: " +duration)
+
+def end():
+    print("Finish")
