bf06b83ed48a35cb2c48137980c7e97e551e9290
bubu14 <romainburet14@gmail.com>
2018-03-15 14:31:15 +0100

    include Camera

--- static/scripts/display.js
+++ static/scripts/display.js
@@ -8,6 +8,7 @@ function teachPage()
 {
     $("div[data-nav='manuel']").removeClass("is-hidden");
     $("div[data-nav='automatic']").addClass("is-hidden");
+    $("div[data-nav='joystick']").addClass("is-hidden");
 
 }
 
@@ -15,5 +16,13 @@ function automaticPage()
 {
     $("div[data-nav='automatic']").removeClass("is-hidden");
     $("div[data-nav='manuel']").addClass("is-hidden");
+    $("div[data-nav='joystick']").addClass("is-hidden");
 
+}
+
+function joystickPage()
+{
+    $("div[data-nav='joystick']").removeClass("is-hidden");
+    $("div[data-nav='manuel']").addClass("is-hidden");
+    $("div[data-nav='automatic']").addClass("is-hidden");
 }
