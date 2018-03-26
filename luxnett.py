b89f63738bd534958a6bb19ddce2fed5785db483
bubu14 <romainburet14@gmail.com>
2018-03-26 17:29:45 +0200

    PiCam

--- luxnett.py
+++ luxnett.py
@@ -24,7 +24,7 @@ def gen(camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-@app.route('/video_feed')
+@app.route('/video_feed/')
 def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
     return Response(gen(Camera()),
