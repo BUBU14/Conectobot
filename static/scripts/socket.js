f7a76257bef0a192d411fe9065c4ff92d035c639
bubu14 <romainburet14@gmail.com>
2018-03-27 11:30:23 +0200

    WERBSOCKET

--- static/scripts/socket.js
+++ static/scripts/socket.js
@@ -1,8 +1,4 @@
 export function init() {
     var socket = io.connect('http://' + document.domain + ':' + location.port);
-
-    socket.on('event', function () {
-        socket.emit('connect', {data: ' Connected'});
-    });
     return socket
 }
