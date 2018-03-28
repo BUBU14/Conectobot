06a46d4389242a963300b92142a344cfc4c54fe8
bubu14 <romainburet14@gmail.com>
2018-03-28 13:00:36 +0200

    socket

--- luxnett.py
+++ luxnett.py
@@ -27,7 +27,7 @@ def gen(camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-@app.route('video_feed')
+@socketio.on('video_feed')
 def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
     return Response(gen(Camera()),
